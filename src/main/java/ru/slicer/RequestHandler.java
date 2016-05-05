5fe9ce6168b1d963b1887d30b52685824dc0afb2
Maksim Gumerov <maxim.gumerov@massmo.ru>
2016-05-05 11:28:51 +0500

    fdsfd

--- src/main/java/ru/slicer/RequestHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.slicer;
-
-import javax.json.JsonObject;
-import javax.websocket.Session;
-
-public interface RequestHandler {
-    void processRequest(String sequence_id, JsonObject data, Session session, Emitter emitter) throws ExpectedException;
-}
